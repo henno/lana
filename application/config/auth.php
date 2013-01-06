574f8e5bc4c0794da64368dced689d6f496e825d
jaakbusin <jaakbushin@gmail.com>
2013-01-06 12:12:45 +0200

    Fix authentication module driver name

--- application/config/auth.php
+++ application/config/auth.php
@@ -2,7 +2,7 @@
 
 return array(
 
-    'driver'       => 'File',
+    'driver'       => 'ORM',
     'hash_method'  => 'sha256',
     'hash_key'     => '1PTLzfEAutlI',
     'lifetime'     => 1209600,
